cde9cf2abd22616ea3d37a9d1318695c8476cf19
Pedro Garcia <garcia.ph@gmail.com>
2017-09-30 22:50:48 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+jQuery Form Machine Plugin
+=========================
+
+A plugin for jQuery that deals with multi-state forms, with a mechanism that works like "steps".
+It can make simple sequential forms, with incrementing steps or more complex forms with states.
+
+## TODO
+
+- Demo
+- Usage
---
+++ js/jquery.form-machine.js
@@ -0,0 +1,17 @@
+/*
+	Form Machine Plugin for jQuery (by Pedro Garcia)
+	Version: 0.1
+	http://phgarcia.com
+	This plugin is offered under the MIT license.
+	Copyright (c) 2017 Pedro Henrique Romaoli Garcia, http://phgarcia.mit-license.org
+*/
+
+(function ($) {
+
+    $.fn.formMachine = function (options) {
+        var settings = $.extend({
+
+        });
+    };
+
+}(jQuery));

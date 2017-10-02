9538be03b12f16b27528c36329255e49854332e8
Pedro Garcia <garcia.ph@gmail.com>
2017-10-02 14:52:41 -0300

    Simple machine behavior implemented

--- js/jquery.form-machine.js
+++ js/jquery.form-machine.js
@@ -10,8 +10,78 @@
 
     $.fn.formMachine = function (options) {
         var settings = $.extend({
-
+            stateSelector: '.form-state',
+            nextSelector : '.form-next',
+            prevSelector : '.form-prev',
+            fadeSpeed    : 300,
+            multiple     : false,
+            before       : null,
+            callback     : null
         });
+        
+        var $form   = this;
+        var $states = $(settings.stateSelector);
+        var $next   = $(settings.nextSelector);
+        var $prev   = $(settings.prevSelector);
+        
+        var stateData = $(settings.stateSelector).map(function() {
+            return $(this).css('display', 'none').data('state-index');
+        }).get();
+        
+        if (!settings.multiple) {
+            // Simple machine behavior
+            var end   = Math.max.apply(Math, stateData);
+            var start = Math.min.apply(Math, stateData);
+            var curr_state = start
+            
+            getState(curr_state).removeAttr('style');
+            $prev.prop('disabled', true);
+            
+            $next.click(function() {
+                if (settings.before) 
+                    settings.before();
+                
+                $prev.prop('disabled', false);
+                
+                getState(curr_state).fadeOut(settings.fadeSpeed, function() {
+                    curr_state++;
+                    getState(curr_state).fadeIn(settings.fadeSpeed);
+                    
+                    if (curr_state == stateData[stateData.length - 1])
+                        $next.prop('disabled', true);
+                    
+                    if (settings.callback) 
+                        settings.callback();
+                });
+            });
+            
+            $prev.click(function() {
+                if (settings.before) 
+                    settings.before();
+                
+                $next.prop('disabled', false);
+                
+                getState(curr_state).fadeOut(settings.fadeSpeed, function() {
+                    curr_state--;
+                    getState(curr_state).fadeIn(settings.fadeSpeed);
+                    
+                    if (curr_state == stateData[0])
+                        $prev.prop('disabled', true);
+                    
+                    if (settings.callback) 
+                        settings.callback(); 
+                });
+            });
+        } else {
+            // Complex machine behavior
+            
+        }
+        
+        $form.addClass('machined');
     };
-
-}(jQuery));
+    
+    function getState(index) {
+        return $('div').find('[data-state-index="' + index + '"]');
+    }
+    
+} (jQuery));
